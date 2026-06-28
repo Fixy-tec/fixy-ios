2703aedfbc9b6c34ac161ee09488b3320b879cbd
YordanDeVp <yordanyrsm@gmail.com>
2026-06-28 10:00:15 -0500

    url google

--- fixy/fixy/App/fixyApp.swift
+++ fixy/fixy/App/fixyApp.swift
@@ -2,16 +2,22 @@
 //  fixyApp.swift
 //  fixy
 //
-//  Created by yordan on 17/06/26.
+//  Created by yordan on 18/06/26.
 //
 
 import SwiftUI
+import Supabase
 
 @main
 struct fixyApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+            // En fixyApp.swift
+            .onOpenURL { url in
+                SupabaseManager.shared.client.auth.handle(url)
+                print("URL de Google procesada")
+            }
         }
     }
 }
