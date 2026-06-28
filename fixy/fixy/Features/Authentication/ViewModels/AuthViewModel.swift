1e4e432e92f8fec0a25843e3da3164be075e5765
YordanDeVp <yordanyrsm@gmail.com>
2026-06-28 11:38:58 -0500

    update func signwithgoogle

--- fixy/fixy/Features/Authentication/ViewModels/AuthViewModel.swift
+++ fixy/fixy/Features/Authentication/ViewModels/AuthViewModel.swift
@@ -41,26 +41,26 @@ final class AuthViewModel {
         
         self.isLoading = false
     }
- 
-    // En AuthViewModel.swift
+    
     func signInWithGoogle() async {
-        self.isLoading = true
-        self.errorMessage = nil
-        
-        do {
-            let url = try SupabaseManager.shared.client.auth.getOAuthSignInURL(
-                provider: .google,
-                redirectTo: URL(string: "fixy://login-callback")
-            )
+            self.isLoading = true
+            self.errorMessage = nil
             
-            await MainActor.run {
-                UIApplication.shared.open(url)
+            do {
+                let url = try SupabaseManager.shared.client.auth.getOAuthSignInURL(
+                    provider: .google,
+                    redirectTo: URL(string: "fixy://login-callback"),
+                    queryParams: [(name: "prompt", value: "select_account")]
+                )
+                
+                await MainActor.run {
+                    UIApplication.shared.open(url)
+                }
+            } catch {
+                print("Error al generar URL de Google: \(error)")
+                self.errorMessage = "No se pudo conectar con Google."
             }
-        } catch {
-            print("Error al generar URL de Google: \(error)")
-            self.errorMessage = "No se pudo conectar con Google."
+            
+            self.isLoading = false
         }
-        
-        self.isLoading = false
-    }
 }
